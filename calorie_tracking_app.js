3658c125ec611e77a146f3e85b2665eb8cd750a2
Ian A <ian4amoroso@gmail.com>
2022-10-14 13:34:13 +0800

    Home Assignment: Variables and Data Types.

---
+++ calorie_tracking_app.js
@@ -0,0 +1,9 @@
+let minutesCycling = 30;
+let daysCycling = 15;
+let caloriesBurnedPerDay = 225;
+
+let totalCaloriesBurned = caloriesBurnedPerDay * daysCycling;
+
+console.log(`Great work, Sam! After ${minutesCycling} minutes of cycling everyday for ${daysCycling} days, you may lose a total of ${totalCaloriesBurned} calories`);
+
+
---
+++ saving_app.js
@@ -0,0 +1,6 @@
+let goalSavingsAmount = 10000;
+let moneySaved = 7500;
+
+let amountLeftToSave = (goalSavingsAmount - moneySaved )/ 100;
+
+console.log(`Thank you for your discipline and hardwork, Sam! You are now ${amountLeftToSave}% away from your goal of saving P${goalSavingsAmount}.`);
